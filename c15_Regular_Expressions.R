3e596ad341a2db9e1f30c986ac248ef575919b2d
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2022-12-02 16:36:47 +0100

    chapter 15 started

---
+++ c15_Regular_Expressions.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(babynames)
+
+curr_pkgs <- read_delim("data/curr_contr.txt", delim = "\t", na = "-")
+
+# show different file types
+table(curr_pkgs$Icon)
+
+# remove 
+curr_pkgs |>
+  filter(Icon == "[   ]") |>
+  rename(File=Name) |>
+  mutate(Name_Version=str_extract(File, ".*(?=.tar.gz)")) |>
+  separate(Name_Version, into = c("Name", "Version"), sep = "_") |>
+  #mutate(c(Name, Version)=strsplit(Name_Version, "_")) |>
+  select(Name, Version, File, Last_modified, Size)
+
+
+
