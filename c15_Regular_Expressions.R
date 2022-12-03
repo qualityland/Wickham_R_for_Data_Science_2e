1bf5afac428813eb4656f962c1bd7785d5e90ad7
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2022-12-03 10:42:17 +0100

    wakefield added

--- c15_Regular_Expressions.R
+++ c15_Regular_Expressions.R
@@ -7,13 +7,15 @@ curr_pkgs <- read_delim("data/curr_contr.txt", delim = "\t", na = "-")
 table(curr_pkgs$Icon)
 
 # remove 
-curr_pkgs |>
+curr_pkgs <- curr_pkgs |>
   filter(Icon == "[   ]") |>
   rename(File=Name) |>
   mutate(Name_Version=str_extract(File, ".*(?=.tar.gz)")) |>
   separate(Name_Version, into = c("Name", "Version"), sep = "_") |>
   #mutate(c(Name, Version)=strsplit(Name_Version, "_")) |>
   select(Name, Version, File, Last_modified, Size)
+
+curr_pkgs
 
 
 
