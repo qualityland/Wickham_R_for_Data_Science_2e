836d158f1717a9e77902b6525251c911db0e9401
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2022-12-07 13:22:01 +0100

    current versions of contributed packages

--- c15_Regular_Expressions.R
+++ c15_Regular_Expressions.R
@@ -37,3 +37,38 @@ eb_pkgs <- eb_pkgs |>
   arrange(Name, Version)
 
 eb_pkgs
+
+
+#####
+# On command line execute this:
+# curl -s https://cran.r-project.org/src/contrib/ | grep -o 'href=".*">' \\
+#   | sed -e "s/href=\"//g" | sed -e "s/\"\>/,/g" |grep ".tar.gz" \\
+#   | sed -e "s/\.tar\.gz<\/a><\/td>.*//g" > curr_cont.txt
+
+
+
+curr_pkgs <- read_csv("data/curr_cont.txt")
+# remove directories and textfiles and add package name and version
+curr_pkgs |>
+  separate(Name_Version, into = c("Name", "Version"), sep = "_")
+  
+  filter(Icon == "[   ]") |>
+  rename(File=Name) |>
+  mutate(Name_Version=str_extract(File, ".*(?=.tar.gz)")) |>
+  separate(Name_Version, into = c("Name", "Version"), sep = "_") |>
+  filter(!is.na(Name)) |>
+  select(Name, Version, File, Last_modified, Size) |> 
+  arrange(Name, Version)
+
+
+
+#####
+library(RCurl)
+url <- 'https://cran.r-project.org/src/contrib/'
+filenames = getURL(url, ftp.use.epsv = FALSE, dirlistonly = TRUE)
+
+
+# Deal with newlines as \n or \r\n. (BDR)
+# Or alternatively, instruct libcurl to change \n’s to \r\n’s for us with crlf = TRUE
+# filenames = getURL(url, ftp.use.epsv = FALSE, ftplistonly = TRUE, crlf = TRUE)
+filenames = paste(url, strsplit(filenames, "\r*\n")[[1]], sep = "")
