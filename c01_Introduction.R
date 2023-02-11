61ba7c28400224d6dda550ed3c5db8b14bf1ffe4
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2023-02-11 07:24:00 +0100

    get datasets from a package

--- c01_Introduction.R
+++ c01_Introduction.R
@@ -1,1 +1,8 @@
+# install packages used in the book
 install.packages(c("babynames", "gapminder", "nycflights13", "palmerpenguins"))
+
+# list of data sets in a specific package
+data(package="ggplot2")$results[, "Item"]
+
+# list of data sets available in base R
+sort(data()$results[, "Item"])
