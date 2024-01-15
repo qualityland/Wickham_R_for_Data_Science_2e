a94d88cdddcf5933f43bbc837af4170a9cacf896
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2024-01-15 18:37:16 +0100

    chapter files renamed

--- c00_Introduction.R
+++ c00_Introduction.R
@@ -1,6 +1,14 @@
 # install packages used in the book
 install.packages(c("babynames", "gapminder", "nycflights13", "palmerpenguins"))
 
+install.packages(
+  c("arrow", "babynames", "curl", "duckdb", "gapminder", "ggrepel", "ggridges",
+    "ggthemes", "hexbin", "janitor", "Lahman", "leaflet", "maps",
+    "nycflights13", "openxlsx", "palmerpenguins", "repurrrsive", "tidymodels",
+    "writexl"
+  )
+)
+
 # list of data sets in a specific package
 data(package="ggplot2")$results[, "Item"]
 
